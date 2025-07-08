cd9cbe3ae344dda3a6fde7104f4718583be4e72e
syeda kiswa <syedakiswa710@gmail.com>
2025-07-08 07:10:23 -0700

    rebuild dist folder

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,21 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+// import tailwindcss from '@tailwindcss/vite'
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react(),
+//        tailwindcss(),
+//   ],
+// })
+
+
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),
-       tailwindcss(),
-  ],
+  base: "/", // 👈 important
+  plugins: [react(), tailwindcss()],
 })
-
-
-
